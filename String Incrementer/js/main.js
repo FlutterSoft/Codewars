04416591bb6ae280e8561eac32d92aae357da0ef
Sam Brown <jumpbro@hotmail.com>
2022-03-15 22:06:41 +1000

    string incrementer

---
+++ String Incrementer/js/main.js
@@ -0,0 +1,33 @@
+/*
+Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
+*/
+
+function incrementString(string) {
+    const arr = string.split('')
+    const arrNums = arr.filter( (char) =>{
+        return !isNaN(char)
+    })
+    let num = Number(arrNums.join(''))
+    num += 1
+    let strNum = num.toString()
+    let answer = arr.splice(-2, 2, strNum)
+    console.log(answer)
+}
+
+  incrementString('foobar23')
