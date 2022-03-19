181c3ef20f38e3bdb74d7a0105690e1b4b625b9e
Sam Brown <jumpbro@hotmail.com>
2022-03-19 20:34:37 +1000

    some codewars

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -228,14 +228,128 @@ Note: keep the original order of the names in the output.
 // // }
 // console.log(squareSum([-19,3,-18,0,2,17,-20,-12]))
 
-function countSheeps(arrayOfSheep) {
-  let count = arrayOfSheep.filter( sheep => sheep === true)
-  return count.length
+// function countSheeps(arrayOfSheep) {
+//   let count = arrayOfSheep.filter( sheep => sheep === true)
+//   return count.length
+// }
+
+// console.log(countSheeps([true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]))
+
+
+
+/*
+
+Money, Money, Money
+
+
+
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+Example:
+
+  Let P be the Principal = 1000.00      
+  Let I be the Interest Rate = 0.05      
+  Let T be the Tax Rate = 0.18      
+  Let D be the Desired Sum = 1100.00
+
+
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30
+Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
+
+Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+
+Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+*/
+
+/*
+function calculateYears(principal, interest, tax, desired) {
+  let balance = principal
+  let years = 0
+  let yearlyInterest = 0 
+  let yearlyTax = 0 
+  if (principal === desired){
+    return 0
+  }
+  while (balance < desired){
+    yearlyInterest = interest*balance
+    yearlyTax = yearlyInterest * tax
+    balance += (yearlyInterest - yearlyTax)
+    years++
+    console.log(`Current balance after ${years} years is ${balance}`)
+  }
+  
+  return years
 }
 
-console.log(countSheeps([true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]))
+console.log(calculateYears(1000,0.05,0.18,1100))
+*/
+
+/*
+Count the divisors of a number
+
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+*/
+
+// function getDivisorsCnt(n){
+//   let count = 0
+//   for (let i = 1; i <= n; i++){
+//     if (n % i === 0) {
+//       count++
+//     }
+//   }
+//   return count
+// }
+
+// console.log(getDivisorsCnt(30))
+
+// function breakChocolate(n,m) {
+//   if (n === 1 && m === 1){
+//     return 0
+//   }
+//   else if (n === 0 || m === 0 ){
+//     return 0
+//   }
+//   else {
+//     return (n*m - 1)
+//   }
+// }
+
+function breakChocolate(n,m) {
+
+  return (n <= 0 || m <= 0) ? 0 : (n*m - 1)
+}
+
+console.log(breakChocolate(5,5))
+
+
+function breakChocolate(n,m) {
+  if (n <= 0 || m <= 0){
+    return 0
+  }
+  else {
+    return (n*m - 1)
+  }
+}
