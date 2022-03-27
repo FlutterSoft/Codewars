c592d6d6d00458607e899f52c27ef2601035530f
Sam Brown <jumpbro@hotmail.com>
2022-03-27 13:00:34 +1000

    Codewars 5kyus

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -422,18 +422,47 @@ For example (Input --> Output):
 //   return (Number(cent) + 1)
 // }
 
-function century(year) {
-  // let len = year.toString().length-2
-  let cent = (year / 100)
-  if (year % 100 == 0){
-    console.log('yo')
-    return (Math.floor(cent))
+// function century(year) {
+//   // let len = year.toString().length-2
+//   let cent = (year / 100)
+//   if (year % 100 == 0){
+//     console.log('yo')
+//     return (Math.floor(cent))
     
-  }
-  else {
-    return (Math.floor(cent) +1)
-  }
-}
+//   }
+//   else {
+//     return (Math.floor(cent) +1)
+//   }
+// }
 
 
-console.log(century(1901))
+// console.log(century(1901))
+
+/*
+Thinkful - List and Loop Drills: Lists of lists
+You have a two-dimensional list in the following format:
+
+data = [[2, 5], [3, 4], [8, 7]]
+Each sub-list contains two items, and each item in the sub-lists is an integer.
+
+Write a function process_data()/processData() that processes each sub-list like so:
+
+[2, 5] --> 2 - 5 --> -3
+[3, 4] --> 3 - 4 --> -1
+[8, 7] --> 8 - 7 --> 1
+and then returns the product of all the processed sub-lists: -3 * -1 * 1 --> 3.
+
+For input, you can trust that neither the main list nor the sublists will be empty.
+*/
+
+// function processData(data){
+//   let finalArr = []
+//   data.forEach( (pair) => {
+//     finalArr.push(pair[0] - pair[1])
+//   })
+//   let answer = finalArr.reduce( (acc, sum) => acc*sum, 1)
+//   return answer
+// }
+// processData([[2,5], [3,4], [8,7]])
+
+
