65c8f67f735cb895136111d2aa5cdcb7d39ca26e
Sam Brown <jumpbro@hotmail.com>
2022-06-30 20:03:17 +1000

    Update main.js

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -931,3 +931,17 @@ function cookie(x){
 }
 
 */
+
+/*
+Pirates!! Are the canons ready??
+const cannonsReady = (gunners) => {
+
+  if(Object.values(gunners).includes('nay')){
+     return "Shiver me timbers!"
+  }
+  else{
+    return "Fire!"
+  }
+
+}
+*/
