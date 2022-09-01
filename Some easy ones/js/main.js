213d4eff0904b1f8433a696e9273e5b424f9d1b1
Sam Brown <jumpbro@hotmail.com>
2022-09-01 19:37:36 +1000

    Update main.js

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -1853,3 +1853,13 @@ reverse = function(array) {
   return reversed
 }
 */
+
+/*
+Online RPG: player to qualifying stage?
+
+
+function playerRankUp (points) {
+  return points >= 100 ? "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." : false;
+}
+
+*/
