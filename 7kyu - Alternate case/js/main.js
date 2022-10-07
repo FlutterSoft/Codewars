bc401b4226670c4d686f71abb20c00c3e0e8ec7f
Sam Brown <jumpbro@hotmail.com>
2022-10-08 08:19:56 +1000

    Alternate Case

---
+++ 7kyu - Alternate case/js/main.js
@@ -0,0 +1,8 @@
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+
+function alternateCase(s) {
+  return s.split('').map(letter => {
+    return letter == letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()
+  }).join('')
+}
