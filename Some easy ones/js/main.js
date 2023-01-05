d6a4c3c300e7f17dc05c963df39ec59bc5786d40
Sam Brown <jumpbro@hotmail.com>
2023-01-05 20:20:06 +1000

    Update main.js

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -2037,3 +2037,13 @@ function validateHello(greetings) {
   return res
 }
 */
+
+
+/*
+Enumerable Magic #4 - True for None?
+Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+function none(arr, fun){
+  return arr.every( x => fun(x) != true)
+}
+
+*/
