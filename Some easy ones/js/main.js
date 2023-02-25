837533e07a7fe137b2a2e3257cc7986f2f9c20e2
Sam Brown <jumpbro@hotmail.com>
2023-02-25 18:07:35 +1000

    Update main.js

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -2062,3 +2062,10 @@ function excludingVatPrice(price){
   return Math.round((price/1.15) * 100) / 100
 }
 */
+
+
+// 8kyu - Push a hash/an object into array
+// You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+// items = []
+// items.push({a: "b", c: "d"})
