224d9e49d1d79bc5be1bcddd8bb49ca7d1885af0
Sam Brown <jumpbro@hotmail.com>
2023-02-26 18:08:47 +1000

    Update main.js

--- Some easy ones/js/main.js
+++ Some easy ones/js/main.js
@@ -2069,3 +2069,22 @@ function excludingVatPrice(price){
 
 // items = []
 // items.push({a: "b", c: "d"})
+
+// 8kyu - Barking mad
+// Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+// snoopy.bark(); // return "Woof"
+// scoobydoo.bark(); // undefined
+
+// function Dog (breed) {
+//   this.breed = breed;
+//   this.bark = function() {
+//     return "Woof"
+//   }
+// }
+
+// var snoopy = new Dog("Beagle");
+
+// var scoobydoo = new Dog("Great Dane");
+
+
